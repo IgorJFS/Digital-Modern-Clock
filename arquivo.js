0a00e9ef362e620d89b0aabf84733dc17b2e57d9
Igor Moraes <igormpaz@gmail.com>
2025-02-12 18:45:16 -0300

    Update arquivo.js

--- arquivo.js
+++ arquivo.js
@@ -1,12 +1,68 @@
+let timeFormat = localStorage.getItem("timeFormat") || "24";
+
+switchTimeFormat(timeFormat)
+
 function updateClock(){
     const now = new Date();
-    const hours = now.getHours().toString().padStart(2,0);
+    let hours = now.getHours().toString().padStart(2,0);
     const minutes = now.getMinutes().toString().padStart(2,0);
     const seconds = now.getSeconds().toString().padStart(2,0);
-    const horario = `${hours}:${minutes}:${seconds}`
-    document.getElementById("clock").textContent = horario
 
+    if (timeFormat === "12"){
+        hours = hours % 12 || 12;
+    }
+    const horario = `${hours.toString().padStart(2, '0')}:${minutes}:${seconds}`;
+    document.getElementById("clock").textContent = horario;
+    
+
+    
+    highlightStage(now.getHours())
+    highlightCurrentDay(now)
 }
+
+function highlightCurrentDay(now){
+    const dayIndex = now.getDay()
+
+    document.querySelectorAll(".day").forEach(day => {
+        day.classList.remove("active")
+    })
+    const dayIds = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+    document.getElementById(dayIds[dayIndex]).classList.add("active");
+}
+
+function highlightStage(hours){
+    const amElement = document.querySelector(".stage:nth-child(1)");
+    const pmElement = document.querySelector(".stage:nth-child(2)");
+    
+    amElement.classList.remove("active")
+    pmElement.classList.remove("active")
+
+    if (hours < 12){
+        amElement.classList.add("active");
+    }else{
+        pmElement.classList.add("active")
+    }
+}
+
+function switchTimeFormat(format){
+    timeFormat = format
+
+    localStorage.setItem("timeFormat", timeFormat)
+
+    document.querySelectorAll(".typeOfTime").forEach(button => {
+        button.classList.remove("active")
+    })
+    if (format === "24"){
+        document.getElementById("military").classList.add("active");
+        document.getElementById("stage-container").style.display = "none";
+    } else{
+        document.getElementById("normal").classList.add("active");
+        document.getElementById("stage-container").style.display = "block";
+    }
+    updateClock()
+}
+
+
 updateClock();
 setInterval(updateClock,1000);
 
